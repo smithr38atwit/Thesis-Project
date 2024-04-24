f8a8bca2d021a46d35646bfad6c019fbeb47a180
Ryan Smith <smithr38@wit.edu>
2024-04-24 15:59:20 -0400

    added "show" option

--- seac/evaluate.py
+++ seac/evaluate.py
@@ -1,16 +1,16 @@
 import gym
+import rware
 import torch
 from a2c import A2C
 from wrappers import Monitor, RecordEpisodeStatistics, TimeLimit
 
-import rware
-
-model_name = "model_no_people"
-path = f"/home/lambda10/rsmith_thesis/Thesis-Project/models/{model_name}"
+model_name = "base_40k_2"
+path = f"C:/Users/smithr38/Code/School/Thesis-Project/models/{model_name}"
 env_name = "rware-tiny-2ag-v1"
 time_limit = 500  # 25 for LBF
 
 EPISODES = 5
+SHOW = True
 
 env = gym.make(env_name)
 agents = [
@@ -20,9 +20,11 @@ agents = [
 for agent in agents:
     agent.restore(path + f"/agent{agent.agent_id}")
 
+total_infos = {}
 for ep in range(EPISODES):
     env = gym.make(env_name)
-    env = Monitor(env, f"videos/{model_name}/video_ep{ep+1}", mode="evaluation")
+    if SHOW:
+        env = Monitor(env, f"videos/test/{model_name}_wp/video_ep{ep+1}", mode="evaluation")
     env = TimeLimit(env, time_limit)
     env = RecordEpisodeStatistics(env)
 
@@ -33,10 +35,14 @@ for ep in range(EPISODES):
         obs = [torch.from_numpy(o) for o in obs]
         _, actions, _, _ = zip(*[agent.model.act(obs[agent.agent_id], None, None) for agent in agents])
         actions = [a.item() for a in actions]
-        env.render()
+        if SHOW:
+            env.render()
         obs, _, done, info = env.step(actions)
     obs = env.reset()
-    print("--- Episode Finished ---")
-    print(f"Episode rewards: {sum(info['episode_reward'])}")
-    print(info)
-    print(" --- ")
+    total_infos
+    if SHOW:
+        print("--- Episode Finished ---")
+        print(f"Episode rewards: {sum(info['episode_reward'])}")
+        print(f"Total Collisions: {sum(info['collisions'])}")
+        print(info)
+        print(" --- ")
