dec95a09bef3c225bda9cbd6fdf8cc980e18ac10
Ryan Smith (lambda10) <smithr38@wit.edu>
2024-05-08 17:53:14 -0400

    change from qmix to mappo

--- exec_train.py
+++ exec_train.py
@@ -4,23 +4,23 @@ from enum import Enum
 
 class Algs(Enum):
     SEAC = 0
-    Q_LEARN = 1
+    MAPPO = 1
 
 
-ALGORITHM = Algs.Q_LEARN
+ALGORITHM = Algs.SEAC
 EVAL = False
 
 match ALGORITHM:
     case Algs.SEAC:
         os.system(
-            "python seac/train.py with env_name=rware-tiny-2ag-v1 num_env_steps=20000000.0 time_limit=500 transfer=False"
+            "python seac/train.py with env_name=rware-small-4ag-v1 num_env_steps=20000000.0 time_limit=500 transfer=True eval_interval=None"
         )
-    case Algs.Q_LEARN:
+    case Algs.MAPPO:
         if EVAL:
             os.system(
                 'python epymarl-main/src/main.py --config=qmix --env-config=gymma with env_args.key="rware:rware-tiny-2ag-v1" evaluate=True'
             )
         else:
             os.system(
-                'python epymarl-main/src/main.py --config=qmix --env-config=gymma with env_args.key="rware:rware-tiny-2ag-v1"'
+                'python epymarl-main/src/main.py --config=mappo --env-config=gymma with env_args.key="rware:rware-tiny-2ag-v1"'
             )
