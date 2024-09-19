12a80c504102ecb1fd714c00323d0d783ebc44f9
Ryan Smith <smithr38@wit.edu>
2024-09-19 18:32:09 -0400

    prevent files from being overwritten when saving

--- seac/evaluate.py
+++ seac/evaluate.py
@@ -19,9 +19,9 @@ env_name = "rware-tiny-2ag-v1"
 time_limit = 500  # 25 for LBF
 
 GSHEET_ROW = None
-EPISODES = 1
-RECORD = True
-RECORD_OUTLIERS = False
+EPISODES = 1000
+RECORD = False
+RECORD_OUTLIERS = True
 SAVE_STATS = True
 
 env = gym.make(env_name)
@@ -42,6 +42,7 @@ stats = {
         "env": env_name,
         "time_limit": time_limit,
         "num_episodes": EPISODES,
+        "num_people": env.n_people,
     },
     "averages": {},
     "episodes": [],
@@ -84,12 +85,15 @@ for ep in range(EPISODES):
     all_collisions.append(total_collisions)
     all_deliveries.append(total_deliveries)
 
-    if RECORD_OUTLIERS and (total_reward < 0 or total_collisions > 10 or total_deliveries < 1):
+    save_outlier = RECORD_OUTLIERS and (total_reward < 0 or total_collisions > 10 or total_deliveries < 1)
+
+    if save_outlier:
         outliers[ep] = {"actions": action_list, "seed": seed}
 
     if SAVE_STATS:
         episode_stats = {
             "episode": ep + 1,
+            "seed": seed,
             "rewards": {
                 "individual": list(info["episode_reward"]),
                 "total": total_reward,
@@ -103,6 +107,8 @@ for ep in range(EPISODES):
                 "total": total_deliveries,
             },
         }
+        if save_outlier:
+            episode_stats["actions"] = action_list
         stats["episodes"].append(episode_stats)
 
 
@@ -146,8 +152,14 @@ if SAVE_STATS:
         },
     }
 
-    # Log stats to file
-    with open(f"C:/Users/smithr38/Code/School/Thesis-Project/stats/episode_{ep+1}.json", "w") as f:
+    # Log stats to file with incrementing suffix
+    base_filename = f"C:/Users/smithr38/Code/School/Thesis-Project/stats/{model_name}.json"
+    suffix = 0
+    while os.path.exists(base_filename):
+        suffix += 1
+        base_filename = f"C:/Users/smithr38/Code/School/Thesis-Project/stats/{model_name}_{suffix}.json"
+
+    with open(base_filename, "w") as f:
         json.dump(stats, f, indent=4)
 
 # Print stats
@@ -159,12 +171,16 @@ print("---")
 
 # Save outlier episodes
 if len(outliers) > 0:
+    # Create the directory
+    output_dir = f"C:/Users/smithr38/Code/School/Thesis-Project/videos/{model_name}_outliers"
+    suffix = 0
+    while os.path.exists(output_dir):
+        suffix += 1
+        output_dir = f"C:/Users/smithr38/Code/School/Thesis-Project/videos/{model_name}_outliers_{suffix}"
+    os.makedirs(output_dir)
+
     for i, (ep, outlier) in enumerate(outliers.items()):
         print(f"Rendering outlier: {i + 1} / {len(outliers)}", end="\r")
-
-        # Create the directory if it doesn't exist
-        output_dir = f"C:/Users/smithr38/Code/School/Thesis-Project/videos/{model_name}_outliers"
-        os.makedirs(output_dir, exist_ok=True)
 
         # Initialize the environment
         o_frames = []
@@ -174,10 +190,10 @@ if len(outliers) > 0:
         o_env.seed(outlier["seed"])
         o_env.reset()
 
-        # Render the environment and save animation
+        # Render the environment
         o_frames.append(o_env.render(mode="rgb_array"))
         for action in outlier["actions"]:
             o_env.step(action)
             o_frames.append(o_env.render(mode="rgb_array"))
         o_env.close()
-        save_animation(o_frames, os.path.join(output_dir, f"ep_{ep+1}_outlier.mp4"))
+        save_animation(o_frames, os.path.join(output_dir, f"ep_{ep+1}.mp4"))
